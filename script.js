a9ea20eb699d6af68bec29e8d06ef75a8b607ac8
Kavya26V <150450253+Kavya26V@users.noreply.github.com>
2024-01-08 11:38:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    let menuu=document.querySelector('.ham');
+    let list=document.querySelector('.menu ');
+    menuu.addEventListener('click',function(){
+         list.classList.toggle('show'); 
+    })
+ 
+
+    
